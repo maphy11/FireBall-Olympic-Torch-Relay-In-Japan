32b092380ca430fe508aa015c577c740f8dd0ba3
maphy <syoyosoccer10@gmail.com>
2020-02-01 08:59:15 +0900

    add:rock object

---
+++ FIREBALL OLYMPIC TORCH RELAY IN JAPAN/Assets/Scripts/Rock/RockCore.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockCore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.transform.tag == "Player")
+        {
+            Debug.Log("Ok");
+            transform.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+    }
+}
