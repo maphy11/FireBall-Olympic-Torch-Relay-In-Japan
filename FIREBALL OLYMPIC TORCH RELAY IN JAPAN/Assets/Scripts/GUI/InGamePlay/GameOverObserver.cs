e14a590536fa63d7f79c120c14aff5c648652475
maphy <syoyosoccer10@gmail.com>
2020-02-01 15:03:44 +0900

    Implement GameOver system

---
+++ FIREBALL OLYMPIC TORCH RELAY IN JAPAN/Assets/Scripts/GUI/InGamePlay/GameOverObserver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+
+namespace GUI
+{
+    public class GameOverObserver : MonoBehaviour
+    {
+        [SerializeField] private PlayerCore coreData;
+        [SerializeField] private GameObject GameOverView;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (coreData.isGameOver && !GameOverView.active)
+            {
+                GameOverView.SetActive(true);
+            }
+        }
+    }
+}
