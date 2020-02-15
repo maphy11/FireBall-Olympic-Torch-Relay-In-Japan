2cf6fa208485f459bd1df340f9f867633f4aead3
maphy <syoyosoccer10@gmail.com>
2020-02-16 00:41:53 +0900

    implement a mechanic,When Player melt ice block, Player can't pass through under the blocks.

--- FIREBALL OLYMPIC TORCH RELAY IN JAPAN/Assets/Scripts/Ice/IceAnimation.cs
+++ FIREBALL OLYMPIC TORCH RELAY IN JAPAN/Assets/Scripts/Ice/IceAnimation.cs
@@ -6,20 +6,44 @@ namespace Ice
 {
 
     [RequireComponent(typeof(Animator))]
-    [RequireComponent(typeof(IceCore))]
     public class IceAnimation : MonoBehaviour
     {
-        private IceCore coreData;
+        private List<IceCore> coreDatas = new List<IceCore>();
         private Animator animator;
         void Start()
         {
-            coreData = GetComponent<IceCore>();
+            IceCore parentCoreData = transform.parent.gameObject.GetComponent<IceCore>();
+            IceCore selfCoreData = GetComponent<IceCore>();
+            if (parentCoreData != null)
+            {
+                coreDatas.Add(parentCoreData);
+            }
+            if (selfCoreData != null)
+            {
+                coreDatas.Add(selfCoreData);
+            }
             animator = GetComponent<Animator>();
         }
 
         void Update()
         {
-            animator.SetBool("isMelting", coreData.isMelting);
+            if (coreDatas == null)
+            {
+                return;
+            }
+            foreach (IceCore coreData in coreDatas)
+            {
+                if (!coreData.isMelting)
+                {
+                    continue;
+                }
+                animator.SetBool("isMelting", coreData.isMelting);
+
+            }
+        }
+        private void FinishMelt()
+        {
+            Destroy(this.transform.parent.gameObject);
         }
     }
 }
--- FIREBALL OLYMPIC TORCH RELAY IN JAPAN/Assets/Scripts/Ice/IceCore.cs
+++ FIREBALL OLYMPIC TORCH RELAY IN JAPAN/Assets/Scripts/Ice/IceCore.cs
@@ -7,16 +7,10 @@ namespace Ice
 {
     public class IceCore : MonoBehaviour, IFireSever
     {
-        [SerializeField] protected PlayerCore coreData;
-        [SerializeField] private float coolTime;
-        protected IPlayerState state;
-        static private bool isInCoolTime;
         public bool isMelting { get; protected set; }
         void Start()
         {
             isMelting = false;
-            isInCoolTime = false;
-            state = coreData.GetComponent(typeof(IPlayerState)) as IPlayerState;
         }
 
         // Update is called once per frame
@@ -27,30 +21,7 @@ namespace Ice
 
         public void TouchFire(FireCore fire)
         {
-            if (!isInCoolTime)
-            {
-                isMelting = true;
-            }
-        }
-
-        private void StartMelt()
-        {
-            state.ToWait();
-            isInCoolTime = true;
-        }
-
-        private void FinishMelt()
-        {
-            StartCoroutine("EnterCoolTime");
-            state.ToMove();
-
-        }
-
-        IEnumerator EnterCoolTime()
-        {
-            yield return new WaitForSeconds(coolTime);
-            isInCoolTime = false;
-            Destroy(this.gameObject);
+            isMelting = true;
         }
     }
 }
