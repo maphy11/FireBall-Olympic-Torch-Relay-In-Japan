62828cd69753790ec101bee7e006849408abe097
Jason Chan <m01171849f@edu.teu.ac.jp>
2020-02-18 07:24:30 +0900

    Wwise

--- FIREBALL OLYMPIC TORCH RELAY IN JAPAN/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ FIREBALL OLYMPIC TORCH RELAY IN JAPAN/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,15 +13,28 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID BGM = 412724365U;
-        static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID BGM_AFRICA = 1544522210U;
+        static const AkUniqueID BGM_ARASKA = 2120438165U;
+        static const AkUniqueID BGM_CLEAR = 155738951U;
+        static const AkUniqueID BGM_GREECE = 1937211529U;
+        static const AkUniqueID BGM_START = 2985635692U;
+        static const AkUniqueID FOX_FOOTSTEPS = 648125588U;
+        static const AkUniqueID FOX_JUMP = 1783467547U;
+        static const AkUniqueID FOX_MELT = 3685664337U;
+        static const AkUniqueID PAUSE = 3092587493U;
+        static const AkUniqueID RETURN = 3859834159U;
+        static const AkUniqueID STAGE_FOUNTAIN = 671713024U;
+        static const AkUniqueID STAGE_WATERDROP = 990763382U;
+        static const AkUniqueID UI_BACK = 2024222415U;
+        static const AkUniqueID UI_RETRY = 3293696140U;
+        static const AkUniqueID UI_SELECT = 2774129122U;
+        static const AkUniqueID UI_START = 1219048826U;
     } // namespace EVENTS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID BGM = 412724365U;
-        static const AkUniqueID SE = 1584861537U;
+        static const AkUniqueID MAIN = 3161908922U;
     } // namespace BANKS
 
     namespace BUSSES
